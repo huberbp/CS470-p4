de71f2ce7760bac9789ea716cbdb77f56bd387bb
huberbp <huberbp@dukes.jmu.edu>
2022-04-13 21:45:50 -0400

    Realized mistake, and corrected for apropriate structure

--- dht.c
+++ dht.c
@@ -14,8 +14,6 @@
 
 #include "dht.h"
 
-#define MAX_LOCAL_PAIRS 65536
-
 /*
  * Private module variable: current process ID (MPI rank)
  */
@@ -27,37 +25,12 @@ static int rank;
 static int nprocs;
 
 /*
- * Local variable to keep track of the # of pairs on each node
+ * Server thread work-loop.  It will await requests and then 
+ * dispatch calls to 
  */
-static size_t pair_count;
-
-/*
- * Key-Value pair structure. For reference, MAX_KEYLEN is 64 and defined in 
- * "local.h".
- */
-typedef struct key_value_pair {
-    char key[MAX_KEYLEN];
-    long value;
-} kv_pair;
-
-/*
- * Private module variable: array that stores all local key-value pairs
- *
- * NOTE: the pairs are stored lexicographically by key for cleaner output
- */
-static kv_pair *kv_pairs;
-
-/*
- * given a key name, return the distributed hash table owner
- * (uses djb2 algorithm: http://www.cse.yorku.ca/~oz/hash.html)
- */
-int hash(const char *name)
+void* server_loop (void* arg)
 {
-    unsigned hash = 5381;
-    while (*name != '\0') {
-        hash = ((hash << 5) + hash) + (unsigned)(*name++);
-    }
-    return hash % nprocs;
+    
 }
 
 int dht_init()
@@ -72,17 +45,12 @@ int dht_init()
         exit(EXIT_FAILURE);
     }
 
-    // Assign each process their rank
+    // This will assign each process their rank correctly.
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    // Get the total number of processes
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    // Set the number of pairs present in each MPI Process to 0
-    pair_count = 0;
-
-    // Dynamically allocate our local kv_pair arrays in each MPI process
-    kv_pairs = (kv_pair*) malloc(MAX_LOCAL_PAIRS * sizeof(kv_pair));
+    // This will perform local initialization for the data structures
+    local_init();
     return rank;
 }
 
@@ -110,10 +78,6 @@ void dht_destroy(FILE *output)
 {
     local_destroy(output);
 
-    // Free our dynamically allocated MPI processes
-    free(kv_pairs);
-
-    // Finalize MPI
     MPI_Finalize();
 }
 
